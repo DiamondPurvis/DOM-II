352493799528f20193cc7850d9a39fe5bc8ba710
DiamondPurvis <purvisdiamond@gmail.com>
2019-02-02 15:08:42 -0500

    Set mouseover event

--- js/index.js
+++ js/index.js
@@ -7,3 +7,9 @@ function hello() {
     alert(`Golly Gee! It's Fun Bus Time!`);
 }
 setTimeout(hello, 500)
+
+// Change color to magenta with mouseover Event
+navigationHeader[0].addEventListener('mouseover', e => {
+    event.target.style.color = "magenta";
+    event.stopPropagation();
+});
